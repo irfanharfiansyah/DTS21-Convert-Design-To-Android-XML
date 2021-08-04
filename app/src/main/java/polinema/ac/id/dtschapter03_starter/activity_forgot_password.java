55bd095691f3682f495841d90747f53df834f13f
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-08-04 22:08:49 +0700

    Add layout forgot password

---
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/activity_forgot_password.java
@@ -0,0 +1,14 @@
+package polinema.ac.id.dtschapter03_starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_forgot_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
