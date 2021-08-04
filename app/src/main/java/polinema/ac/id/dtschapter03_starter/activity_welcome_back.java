dcafc1ab83de977dc882c81713202ece4dc1a4af
irfanharfiansyah <Irfanharfiansyah37@gmail.com>
2021-08-04 21:30:30 +0700

    init project

---
+++ app/src/main/java/polinema/ac/id/dtschapter03_starter/activity_welcome_back.java
@@ -0,0 +1,14 @@
+package polinema.ac.id.dtschapter03_starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_welcome_back extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_back);
+    }
+}
